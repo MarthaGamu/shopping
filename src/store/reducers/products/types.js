4b9c0f5f7091189740950a5cddfba02c94da021c
achamwada <alexanderchamwada@gmail.com>
2020-08-09 11:37:41 +0100

    product reducer

---
+++ src/store/reducers/products/types.js
@@ -0,0 +1,25 @@
+export const FETCH_PRODUCTS = "FETCH_PRODUCTS";
+export const FILTER = "FILTER";
+export const SORT_BY = "SORT_BY";
+
+/**
+ * fetch all
+ * filter
+ * sort by
+ *
+ *
+ *
+ *
+ *
+ * product={
+ * id:
+ * name:
+ * description:
+ * price:
+ * size:
+ * dateAdded:
+ * image:
+ * }
+ *
+ *
+ */
